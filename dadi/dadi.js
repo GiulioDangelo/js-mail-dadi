514b88abb15b5797b319edba5502256da301dd63
GiulioDangelo <dangelogiulio48@gmail.com>
2023-04-17 16:59:10 +0200

    add DOM click function

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -1,15 +1,18 @@
-const playerNum = Math.floor(Math.random() * 6) + 1;
-const cpuNum = Math.floor(Math.random() * 6) + 1;
+const buttonEle = document.getElementById("dice-roll")
 
-  if (playerNum > cpuNum) {
-    console.log('player win')
-  }
-  else if (playerNum == cpuNum) {
-    console.log("it's a draw")
-  }
-  else{
-    console.log('cpu win')
-  }
-
-  console.log(playerNum)
-  console.log(cpuNum)
+buttonEle.addEventListener("click",function(){
+    const playerNum = Math.floor(Math.random() * 6) + 1;
+    const cpuNum = Math.floor(Math.random() * 6) + 1;
+    const playerEle = document.getElementById("player-result").innerHTML=(playerNum)
+    const cpuEle = document.getElementById("cpu-result").innerHTML=(cpuNum)
+    
+    if (playerNum > cpuNum) {
+        document.getElementById("conclusion").innerHTML=('player win')
+    }
+    else if (playerNum == cpuNum) {
+        document.getElementById("conclusion").innerHTML=("it's a draw")
+    }
+    else{
+        document.getElementById("conclusion").innerHTML=('cpu win')
+    }
+})
